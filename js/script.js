218ee2fe606ca86064288f25ded7ec0313e7a662
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-11 16:37:32 +0100

    inserimento axios

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// **Descrizione:**
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// **Bonus**
+// Creare un loading e far comparire gli indirizzi email solamente quando sono stati TUTTI generati.
+
+
+
+const { createApp } = Vue;
+
+//console.log(axios);
+
+createApp({
+
+  data(){
+    return{
+      title: 'email'
+    }
+  },
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail'.then( risultato =>{
+      console.log(risultato);
+    }))
+  }
+}).mount('#app')
