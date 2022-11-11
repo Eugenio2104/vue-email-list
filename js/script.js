12d0e7644ef6a37b47a21e02b07ebd23fceb12f5
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-11-11 17:20:58 +0100

    pausa esercizio

--- js/script.js
+++ js/script.js
@@ -17,13 +17,10 @@ createApp({
   data(){
     return{
       title: 'email',
-      listaMail: ''
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      listaMail: []
     }
   },
-  mounted(){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( risultato => {
-      console.log(risultato.data);
-      this.listaMail = risultato.data.response
-    })
-  },
+  
+
 }).mount('#app');
